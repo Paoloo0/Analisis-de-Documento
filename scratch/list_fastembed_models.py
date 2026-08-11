786602463c6320348a1ccee83555be5e7bda29d8
Paolo <paolo@example.com>
2026-08-11 12:50:57 -0500

    cleanup: remove temporary scratch scripts

--- scratch/list_fastembed_models.py
+++
@@ -1,5 +0,0 @@
-from fastembed import TextEmbedding
-
-print("Supported models in fastembed:")
-for m in TextEmbedding.list_supported_models():
-    print(f"- {m['model']} (dim: {m['dim']}, size_in_GB: {m.get('size_in_GB', 'N/A')}, description: {m.get('description', '')})")
